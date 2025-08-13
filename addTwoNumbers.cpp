01fcb8e1499a40388dbf370ef4f89d6a8560d894
JulianGSouza <0958708@my.scccd.edu>
2025-08-13 12:23:00 -0700

    Add files via upload

---
+++ addTwoNumbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+// Created by Julian Souza on 8/13/2025.
+// addTwoNumbers.cpp/
+int main();
+{
+
+
+int sumOfNums = 0;
+int num1 = 0;
+int num2 = 0;
+
+         cout << "Enter a value for num1";
+         cin >> num1;
+         cout << "Enter a whole number for num2";
+         cin >> num2;
+
+
+        sumOfNums = num1 + num2;
+
+
+            cout << "The sum of " << num1 << " and " << num2 << "is " << sumOfNums;
+            }
