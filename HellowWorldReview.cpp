6f64cef87acd41d8af8cd1c5d2f9a4c82c630ba4
JulianGSouza <0958708@my.scccd.edu>
2025-08-13 11:50:29 -0700

    Add files via upload

---
+++ HellowWorldReview.cpp
@@ -0,0 +1,15 @@
+//HellWorldReview
+//JS 8.13.25
+
+#include <iostream>
+int main()
+{
+    int num1 = 5;
+    int num2 = 10;
+    int sum = num1 + num2;
+    std::cout << "The Value of num1 is " << num1 << std::endl;
+    std::cout << "The Value of num2 is " << num2 << std::endl;
+    std::cout << "The sum of " << num1 << " and " << num2 <<" is " << sum << std::endl;
+
+    return 0;
+}
