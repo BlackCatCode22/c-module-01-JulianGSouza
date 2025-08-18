caf54d1ee065ccf379257445f6785caf4ab1cfb9
JulianGSouza <0958708@my.scccd.edu>
2025-08-18 11:02:19 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//AddTwoInts.cpp
+//J.S. 8.18.25
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1,num2,sumOfNums;
+
+    cout << "Enter two numbers";
+    cin >> num1 >> num2;
+
+    sumOfNums = num1 + num2;
+
+    cout << "Sum of the two numbers = "<< sumOfNums;
+
+    return 0;
+}
