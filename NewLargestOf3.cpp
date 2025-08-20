a68cc7d216d9da6b155eb8475c97bae77fd19e69
JulianGSouza <0958708@my.scccd.edu>
2025-08-20 12:02:54 -0700

    Update NewLargestOf3.cpp

--- NewLargestOf3.cpp
+++ NewLargestOf3.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Welcome to my largest of three ints program." << endl;
+    cout << "Welcome to my adding three ints program." << endl;
 
     int num1,num2,num3,sumOfNums;
     num1 = 0;
@@ -30,19 +30,30 @@ int main()
     cin >> num3;
     cout << "You entered " << num3 << " for num3. " << endl;
 
-    //finding the largest of the three with an if statment
-    if (num1 > num2 && num1 > num3)
+    //finding the largest of the three with an if statement
+    if (num1 > num2)
     {
-        cout << num1 << " is greater than " << num2 << " and " << num3 << endl;
-    }
-    else if (num2 > num1 && num2 > num3)
-    {
-        cout << num2 << " is greater than " << num1 << " and " << num3 << endl;
-    }
+     if(num1 > num3)
+         {
+         cout << "num1 = " << num1 << " and is the largest int." << endl;
+         }
+        else
+            {
+            cout << "num3 = " <<  num3 << " and is the largest int." << endl;
+             }
+     }
     else
     {
-        cout << num3 << " is greater than " << num1 << " and " << num2 << endl;
+        if(num2 > num3)
+        {
+            cout << "num2 = " <<  num2 << " and is the largest int." << endl;
+        }
+        else
+        {
+            cout << "num3 = " <<  num3 << " and is the largest int." << endl;
+        }
     }
+
     return 0;
 
 }
